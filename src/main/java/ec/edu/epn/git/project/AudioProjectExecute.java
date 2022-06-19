d925126f4af549f0c5a4f7deb8ec46bec6f3164e
Francisco Encalada <francisco.encalada01@epn.edu.ec>
2022-06-19 02:25:09 -0500

    HU3 - Play an audio file: Was added a try-catch clause to prevent the problem compatibility of playing Jenkins' media audio.

--- src/main/java/ec/edu/epn/git/project/AudioProjectExecute.java
+++ src/main/java/ec/edu/epn/git/project/AudioProjectExecute.java
@@ -2,27 +2,30 @@ package ec.edu.epn.git.project;
 
 public class AudioProjectExecute {
     public static void main(String[] args) {
-        System.out.println("-- Audio Project Execute --");
-        Explorer explorer = new Explorer();
-        String[] listFiles = explorer.getFilesList();
-        System.out.println("Showing the files' list:");
-        for (String nameFile : listFiles) {
-            System.out.println("\t" + nameFile);
-        }
+        try{
+            System.out.println("-- Audio Project Execute --");
+            Explorer explorer = new Explorer();
+            String[] listFiles = explorer.getFilesList();
+            System.out.println("Showing the files' list:");
+            for (String nameFile : listFiles) {
+                System.out.println("\t" + nameFile);
+            }
 
-        String source = String.valueOf(explorer.directory);
-        String dest = String.valueOf(explorer.directory.resolve("converterOut"));
-        System.out.println("\nThe moving file process has started:");
-        explorer.moveFile("movingFile.txt",source, dest);
-        System.out.println("\tThe file has been moved: " + source + " --> " + dest);
-        explorer.moveFile("movingFile.txt",dest, source);
-        System.out.println("\tThe file has been moved: " + dest + " --> " + source);
+            String source = String.valueOf(explorer.directory);
+            String dest = String.valueOf(explorer.directory.resolve("converterOut"));
+            System.out.println("\nThe moving file process has started:");
+            explorer.moveFile("movingFile.txt",source, dest);
+            System.out.println("\tThe file has been moved: " + source + " --> " + dest);
+            explorer.moveFile("movingFile.txt",dest, source);
+            System.out.println("\tThe file has been moved: " + dest + " --> " + source);
 
-        System.out.println("\nChecking the playing process and the audio compatibility:");
-        explorer.setAudioFileByName("cheer.wav");
-        boolean compatibility = explorer.playAudioFile();
-        if (compatibility) {
-            System.out.println("\tThe file is compatible");
-        }
+            System.out.println("\nChecking the playing process and the audio compatibility:");
+            explorer.setAudioFileByName("cheer.wav");
+            boolean compatibility = explorer.playAudioFile();
+            if (compatibility) {
+                System.out.println("\tThe file is compatible");
+            }
+        }catch (Exception e){}
+
     }
 }
