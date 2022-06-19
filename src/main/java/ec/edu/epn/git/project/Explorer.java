525e468ce1a8209e0f0c0b13e8cdff7f2efd17ec
Francisco Encalada <francisco.encalada01@epn.edu.ec>
2022-06-18 23:10:32 -0500

    HU1 - Visualize the files' list: The getFilesList method was implemented to show the files list. Additionally, some files were added to test the program.

--- src/main/java/ec/edu/epn/git/project/Explorer.java
+++ src/main/java/ec/edu/epn/git/project/Explorer.java
@@ -1,4 +1,47 @@
 package ec.edu.epn.git.project;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Explorer {
+
+    Path directory;
+    public Explorer() {
+        String stringDirectory = System.getProperty("user.dir");
+        Path mainPath = Path.of(stringDirectory);
+        this.directory = mainPath.getParent().resolve("audioLibrary");
+
+        // Checking if the default path is accessible
+        if (!checkPath(directory)) {
+            this.directory = mainPath.resolve("audioLibrary");
+        }
+    }
+
+    private boolean checkPath(Path path) {
+        return Files.exists(path);
+    }
+    public String[] getFilesList() {
+        // Local variables
+        File directoryFile = new File(String.valueOf(directory));
+        File[] filesPathsList = directoryFile.listFiles();
+        ArrayList<String> fileNames = new ArrayList<>();
+
+        String[] error = {"There are no files in the pointing directory\n"};
+
+        if (filesPathsList != null) {
+            // Sorting files
+            Arrays.sort(filesPathsList);
+
+            for (File file : filesPathsList) {
+                fileNames.add(file.getName());
+            }
+
+            return fileNames.toArray(new String[0]);
+        }
+
+        return error;
+    }
 }
--- src/test/java/ec/edu/epn/git/project/ExplorerTest.java
+++ src/test/java/ec/edu/epn/git/project/ExplorerTest.java
@@ -1,7 +1,23 @@
 package ec.edu.epn.git.project;
 
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class ExplorerTest {
 
+    Explorer e;
+    @Before
+    public void starUp() {
+        e = new Explorer();
+    }
+
+    @Test
+    public void given_a_directory_when_returning_the_files_list_then_ok() {
+        String[] expected = {"arc1.aiff", "arc1.au", "arc1.wav", "cheer.wav", "converterOut", "nature.wav" };
+        String[] current = e.getFilesList();
+
+        assertArrayEquals(expected, current);
+    }
 }
